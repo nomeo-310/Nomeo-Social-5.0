22f076ca13e2ff8e40df828c1659eb9869e0fab2
nomeo-310 <onome962@gmail.com>
2024-01-30 11:29:37 +0100

    changes made

--- src/app/api/createProfile/[id]/route.ts
+++ src/app/api/createProfile/[id]/route.ts
@@ -6,12 +6,10 @@ import { formProps } from "@/types/types";
 
 export const PUT = async (request: NextRequest, response: NextResponse, {params}:{params: {id: string}}) => {
   const { username, surname, lastname, gender, status, address, hobbies, otherSocialProfiles, profileCreated, occupation, birthdate, birthday, age, city, state, mobileNumber, bio, interests}:formProps = await request.json();
-
+  const userId = params.id
   const updateData:formProps = { username: username, surname: surname, lastname: lastname, gender: gender, status: status, address: address, hobbies: hobbies, otherSocialProfiles: otherSocialProfiles, profileCreated: profileCreated, occupation: occupation, birthdate: birthdate, birthday: birthday, age: age, city: city, state: state, mobileNumber: mobileNumber, bio: bio, interests: interests }
-
   await connectToDatabase();
 
-  const userId = params.id
 
   try {
     await User.findOneAndUpdate({_id: userId}, {$set: updateData})
